5563d794ae445205c086eff25b2f44a6fd98f6f2
Cyril Pitar <cyrilpitar@orange.fr>
2025-02-07 11:34:32 +0100

    update

--- src/main/java/fr/usmb/m1isc/compilation/tp/Main.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Main.java
@@ -7,16 +7,16 @@ import java_cup.runtime.Symbol;
 public class Main {
 
     public static void main(String[] args) throws Exception  {
-	LexicalAnalyzer yy;
-	if (args.length > 0)
-	    yy = new LexicalAnalyzer(new FileReader(args[0])) ;
-	else
-	    yy = new LexicalAnalyzer(new InputStreamReader(System.in)) ;
-	@SuppressWarnings("deprecation")
-	parser p = new parser (yy);
-	Symbol s = p.parse();
-	Noeud arbre = (Noeud) s.value;
-	arbre.afficher();
+        LexicalAnalyzer yy;
+        if (args.length > 0)
+            yy = new LexicalAnalyzer(new FileReader(args[0])) ;
+        else
+            yy = new LexicalAnalyzer(new InputStreamReader(System.in)) ;
+        @SuppressWarnings("deprecation")
+        parser p = new parser (yy);
+        Symbol s = p.parse();
+        Noeud arbre = (Noeud) s.value;
+		arbre.afficher();
     }
 
 }
